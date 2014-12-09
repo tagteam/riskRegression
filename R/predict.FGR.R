a5df5d3eeb7fb01f89661e6d538b448bb7dbd13b
tag <tag@edab9b15-3c32-4f9a-a569-739a535b425d>
2014-12-09 14:01:27 +0000

    fixed Intercept bug in model.design
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg/pkg/riskRegression@359 edab9b15-3c32-4f9a-a569-739a535b425d

--- R/predict.FGR.R
+++ R/predict.FGR.R
@@ -6,6 +6,7 @@ predict.FGR <- function(object,newdata,times,...){
     rhs <- as.formula(delete.response(object$terms))
     EHF <- prodlim::model.design(rhs,
                                  newdata,
+                                 dropIntercept=TRUE,
                                  specialsDesign=TRUE)
     # }}}
     # {{{ covariate design matrices
@@ -16,7 +17,9 @@ predict.FGR <- function(object,newdata,times,...){
     # }}}
     args <- list(object=object$crrFit,cov1=cov1,cov2=cov2,...)
     args <- args[!sapply(args,is.null)]
+    ## warning("uuu")
     pred <- do.call("predict.crr",args)
+    ## warning("bbb")
     out <- pred[,-1,drop=FALSE]
     if (!missing(times)){
         tind <- prodlim::sindex(jump.times=pred[,1],eval.times=times)
