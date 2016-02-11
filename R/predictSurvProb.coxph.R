092bf8f162897366f932cf696dfb6c4fab84830c
bozenne <broz@sund.ku.dk>
2016-02-11 16:40:25 +0100

    Add fast computation of the baseline hazard

--- R/predictSurvProb.coxph.R
+++ R/predictSurvProb.coxph.R
@@ -6,24 +6,24 @@ h_theta_breslow_strata <- function(object,time, status, Z,cause){
   
   strataspecials = attr(object$terms,"specials")$strata
   if (is.null(strataspecials)) {
-  eventtimes <- unique(sort(time[status!=0]))
-  h_theta_event <- numeric(length=length(unique(eventtimes)))
-  coef <- object$coef[sort(names(object$coef))]
-  
-  for (i in order(eventtimes)){
-    Ri <- (time>=eventtimes[i])
-    ExpCov <- matrix(NA, nrow=sum(Ri),ncol=1)
-    for (j in 1:sum(Ri)) { 
-      ExpCov[j] <- exp(coef%*%t(Z[Ri,])[,j])
+    eventtimes <- unique(sort(time[status!=0]))
+    h_theta_event <- numeric(length=length(unique(eventtimes)))
+    coef <- object$coef[sort(names(object$coef))]
+    
+    for (i in order(eventtimes)){
+      Ri <- (time>=eventtimes[i])
+      ExpCov <- matrix(NA, nrow=sum(Ri),ncol=1)
+      for (j in 1:sum(Ri)) { 
+        ExpCov[j] <- exp(coef%*%t(Z[Ri,])[,j])
+      }
+      h_theta_event[i] <- sum((time==eventtimes[i]) & (status==cause))/sum(ExpCov)  
     }
-    h_theta_event[i] <- sum((time==eventtimes[i]) & (status==cause))/sum(ExpCov)  
-  }
-
-  h_theta <- h_theta_event[match(unique(sort(time)),eventtimes)]
-  h_theta[is.na(h_theta)] <- 0
-  H_theta <- cumsum(h_theta)
-  new_basehaz <- data.frame(hazard = H_theta, time = sort(unique(time)))
-  return(new_basehaz)
+    
+    h_theta <- h_theta_event[match(unique(sort(time)),eventtimes)]
+    h_theta[is.na(h_theta)] <- 0
+    H_theta <- cumsum(h_theta)
+    new_basehaz <- data.frame(hazard = H_theta, time = sort(unique(time)))
+    return(new_basehaz)
   } 
   else {
     new_basehaz <- NULL
@@ -32,29 +32,29 @@ h_theta_breslow_strata <- function(object,time, status, Z,cause){
     mod <- model.frame(object) 
     Terms <- attr(object$terms, "term.labels")
     BaseVar <- Terms[ Terms != stratalabels ]
-  
+    
     for (s in 1:length(strata)){
       Si <- mod[stratalabels][,1]==levels(mod[stratalabels][,1])[s]
       strataeventtimes <- unique(sort(time[status!=0 & Si]))
       stratatime <- time[Si]
       h_theta_event <- numeric(length=length(unique(strataeventtimes)))
-    for (i in order(strataeventtimes)){
-      Ri <- (stratatime>=strataeventtimes[i])
-      ExpCov <- matrix(NA, nrow=sum(Ri),ncol=1)
-      for (j in 1:sum(Ri)) { 
-        if(length(BaseVar)!=1){
-        ExpCov[j] <- exp(object$coef%*%t((Z[Si,BaseVar][Ri,][j,])))}
-        else {
-          ExpCov[j] <- exp(object$coef%*%t((Z[Si,BaseVar][Ri][j])))
+      for (i in order(strataeventtimes)){
+        Ri <- (stratatime>=strataeventtimes[i])
+        ExpCov <- matrix(NA, nrow=sum(Ri),ncol=1)
+        for (j in 1:sum(Ri)) { 
+          if(length(BaseVar)!=1){
+            ExpCov[j] <- exp(object$coef%*%t((Z[Si,BaseVar][Ri,][j,])))}
+          else {
+            ExpCov[j] <- exp(object$coef%*%t((Z[Si,BaseVar][Ri][j])))
+          }
         }
+        h_theta_event[i] <- sum((stratatime==strataeventtimes[i]) & (status[Si]==cause))/sum(ExpCov)
       }
-      h_theta_event[i] <- sum((stratatime==strataeventtimes[i]) & (status[Si]==cause))/sum(ExpCov)
-    }
-    h_theta <- h_theta_event[match(unique(sort(stratatime)),strataeventtimes)]
-    h_theta[is.na(h_theta)] <- 0
-    H_theta <- cumsum(h_theta)
-    new_basehaz <- rbind(new_basehaz, data.frame(hazard = H_theta,
-        time = sort(unique(stratatime)), strata=levels(mod[stratalabels][,1])[s]))
+      h_theta <- h_theta_event[match(unique(sort(stratatime)),strataeventtimes)]
+      h_theta[is.na(h_theta)] <- 0
+      H_theta <- cumsum(h_theta)
+      new_basehaz <- rbind(new_basehaz, data.frame(hazard = H_theta,
+                                                   time = sort(unique(stratatime)), strata=levels(mod[stratalabels][,1])[s]))
     }
     return(new_basehaz)
   }
@@ -65,9 +65,9 @@ h_theta_breslow_strata <- function(object,time, status, Z,cause){
 #
 
 predictSurvProb.coxph<- function (object, times, newdata, time, status, Z, cause) {
-    NewBasehaz <- h_theta_breslow_strata(object, time,status, Z,cause)
-    p <- matrix(NA, nrow=nrow(newdata), ncol=length(times))
-    strataspecials = attr(object$terms,"specials")$strata
+  NewBasehaz <- h_theta_breslow_strata(object, time,status, Z,cause)
+  p <- matrix(NA, nrow=nrow(newdata), ncol=length(times))
+  strataspecials = attr(object$terms,"specials")$strata
   
   if (is.null(strataspecials)){
     time.index = sort(sindex(jump.times=NewBasehaz$time,eval.times=times))
@@ -84,19 +84,18 @@ predictSurvProb.coxph<- function (object, times, newdata, time, status, Z, cause
     Terms <- attr(object$terms, "term.labels")
     BaseVar <- Terms[ Terms != stratalabels ]
     
-  for (i in 1:nrow(newdata)) {
-    Base <- NewBasehaz[NewBasehaz$strata==eval(parse(text = stratalabels), newdata)[i],]
-    newdata$strata <- eval(parse(text = stratalabels), newdata)
-    time.index = sort(sindex(jump.times=Base$time,eval.times=times))
-                     
-    for (j in 1:length(times)) { 
-      t <-  sort(times)[j]   
-      Si <- (newdata$strata == eval(parse(text = stratalabels), newdata)[i])
-      p[i,j] <- exp(-c(0,Base[,1])[1+time.index[j]])^exp(sum(newdata[i,BaseVar]*object$coef))
+    for (i in 1:nrow(newdata)) {
+      Base <- NewBasehaz[NewBasehaz$strata==eval(parse(text = stratalabels), newdata)[i],]
+      newdata$strata <- eval(parse(text = stratalabels), newdata)
+      time.index = sort(sindex(jump.times=Base$time,eval.times=times))
+      
+      for (j in 1:length(times)) { 
+        t <-  sort(times)[j]   
+        Si <- (newdata$strata == eval(parse(text = stratalabels), newdata)[i])
+        p[i,j] <- exp(-c(0,Base[,1])[1+time.index[j]])^exp(sum(newdata[i,BaseVar]*object$coef))
       }
+    }
+    
+    return(p)
   }
-                    
-  return(p)
 }
-}
-
--- src/h_theta_breslow_strata.cpp
+++ src/h_theta_breslow_strata.cpp
@@ -11,144 +11,56 @@ const double CST_EPSILON = std::numeric_limits < double>::epsilon();
 NumericVector caclEventtimes_cpp(const IntegerVector& status, const NumericVector& alltimes, int n_patients, int cause);
 NumericVector sortNV_hpp(NumericVector x) ;
 
-//  1 ////////////////////////////////////////////////////////////
 // [[Rcpp::export]]
-List BaselineHazard_cpp(const IntegerVector& status, const NumericVector& alltimes, int cause,
-                        const NumericVector& Xb, NumericVector times){
+List BaselineHazardFast_cpp(const IntegerVector& status, const NumericVector& pattimes, const NumericVector& Xb, int n_patients,
+                            const NumericVector& eventtimes, int cause){
   
-  // NOTE : status, alltimes, cause, Xb must refer to the patients in the same order
+  // NOTE : status, alltimes, Xb must refer to the patients in the same order
+  //        eventtimes must be sorted in ascending order
   
   //// intialisation
-  int n_patients = Xb.size() ; 
-  int n_times = times.size() ; 
-  times = sortNV_hpp(times);
+  // sort status, pattimes, Xb in ascending order relative to pattimes
+  // sort eventtimes in ascending order
+  int n_eventtimes = eventtimes.size();
   
-  // define eventtimes
-  NumericVector eventtimes = caclEventtimes_cpp(status, alltimes, n_patients, cause);
-  eventtimes = sortNV_hpp(eventtimes);
-  int n_eventtimes = eventtimes.size() ; 
-  
-  // prepare Lambda
-  double W = 0, Lambda_tempo = 0;
+  double W, W_next = 0;
   for(int iter_pat = 0 ; iter_pat < n_patients ; iter_pat++){
-    W += exp(Xb[iter_pat]);
+    W_next += exp(Xb[iter_pat]);
   }
-  NumericVector Lambda0(n_times);
+  NumericVector Lambda0(n_eventtimes+1);
   
-  double d;
-  IntegerVector atRisk(n_patients,1);
-  double time_tempo;
-  
-  int iter_time = 0;
-  int iter_eventtime = 0;
-  bool test_eqtime;
-  bool test_inftime;
+  double death;
   
   //// main: loop over eventtimes
+  Lambda0[0] = 0;
   
-  // before the first event
-  while(iter_time < n_times && times[iter_time] < eventtimes[0]){
-    Lambda0[iter_time] = 0;
-    iter_time++;
-  }
+  int iter_pat = 0 ;
   
-  // first to last event
-  while((iter_time < n_times) && (iter_eventtime < n_eventtimes)){
-    time_tempo = eventtimes[iter_eventtime];
+  for( int iter_time = 0 ; iter_time < n_eventtimes ; iter_time ++){
+   W = W_next;
     
-    // update W and d
-    d = 0;
-    for(int iter_pat = 0 ; iter_pat < n_patients ; iter_pat++){
+    // patients with event at time t
+    death = 0;
+    do{
       
-      if(status[iter_pat] == cause && alltimes[iter_pat] == time_tempo){ // dead at time_tempo
-        d ++;
-      } else if(alltimes[iter_pat] < time_tempo && atRisk[iter_pat] == 1){ // no more at risk at time_tempo (already censored or dead)
-        W -= exp(Xb[iter_pat]);
-        atRisk[iter_pat] = 0;
+      if(status[iter_pat] == cause){
+        death ++;
       }
-      
-    }
-    
+      W_next -= exp(Xb[iter_pat]); // no more at risk at for next time (already censored or dead)
+      iter_pat++;
+        
+    }while(iter_pat < n_patients && pattimes[iter_pat] == eventtimes[iter_time]);
+   
     // udpate Lambda0
-    Lambda_tempo += d / W;
+    Lambda0[iter_time+1] = Lambda0[iter_time] + death / W;
     
-    // update Lambda0 if requested time in [ eventtimes[iter_eventtime] ; eventtimes[iter_eventtime + 1] [
-    test_eqtime = times[iter_time] == time_tempo ;
-    test_inftime = (iter_eventtime + 1) < n_eventtimes &&  times[iter_time] < eventtimes[iter_eventtime + 1];
-    
-    while((iter_time < n_times) && (test_eqtime || test_inftime) ){ 
-      Lambda0[iter_time] = Lambda_tempo ;
-      
-      iter_time++;
-      if(iter_time < n_times){
-        test_eqtime = times[iter_time] == time_tempo ;
-        test_inftime = (iter_eventtime + 1) < n_eventtimes &&  times[iter_time] < eventtimes[iter_eventtime + 1];
-      }
-      
-    }
-    
-    // move to next time
-    iter_eventtime++;
   }
-  
-  // after the first event
-  while(iter_time < n_times){
-    Lambda0[iter_time] = Lambda_tempo;
-    iter_time++;
-  }
-  
   
   //// export
+  Lambda0.erase(Lambda0.begin());
+  
   return(List::create(Named("hazard")  = Lambda0,
-                      Named("time")  = times)
+                      Named("time")  = eventtimes)
   );
   
-  
-  //   return(List::create(Named("hazard")  = Lambda0,
-  //                       Named("d")  = d,
-  //                       Named("W")  = W,
-  //                       Named("atRisk")  = atRisk,
-  //                       Named("Lambda_tempo")  = Lambda_tempo,
-  //                       Named("time")  = times,
-  //                       Named("alltimes")  = alltimes, 
-  //                       Named("eventtimes")  = eventtimes));
-}
-
-
-//  2 ////////////////////////////////////////////////////////////
-// [[Rcpp::export]]
-NumericVector caclEventtimes_cpp(const IntegerVector& status, const NumericVector& alltimes, int n_patients, int cause){
-  
-  NumericVector eventtimes(0);
-  bool test_tie;
-  
-  for(int iter_pat=0 ; iter_pat < n_patients ; iter_pat++){
-    
-    if(status[iter_pat] == cause){
-      
-      // check ties
-      test_tie = false;
-      for(int iter_time = 0 ; iter_time < eventtimes.size(); iter_time++){
-        if(eventtimes[iter_time] == alltimes[iter_pat]){
-          test_tie = true;
-          break;
-        }
-      }
-      if(test_tie == false){
-      eventtimes.push_back (alltimes[iter_pat]);
-      }
-      
-    }
-    
-  }
-  
-  return(eventtimes);
-}
-
-//  3 ////////////////////////////////////////////////////////////
-// [[Rcpp::export]]
-NumericVector sortNV_hpp(NumericVector x) {
-  NumericVector y = clone(x);
-  std::sort(y.begin(), y.end());
-  return y;
 }
