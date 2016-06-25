a967498fd010229a19537055ace80ecbf0bb0b45
Thomas Alexander Gerds <tag@biostat.ku.dk>
2016-06-25 10:25:32 +0200

    cleaned up

---
+++ R/plotAUC.R
@@ -0,0 +1,99 @@
+### plotAUC.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds
+## created: Jun 23 2016 (09:19) 
+## Version: 
+## last-updated: Jun 25 2016 (10:24) 
+##           By: Thomas Alexander Gerds
+##     Update #: 19
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+##' Plot AUC curve
+##'
+##' @title Plot AUC curve
+##' @param x Object obtained with \code{Score.list}
+##' @param models Choice of models to plot
+##' @param type Character. Either \code{"score"} to show AUC or \code{"test"} to show differences between AUC.
+##' @param lwd Line width
+##' @param xlim Limits for x-axis
+##' @param ylim Limits for y-axis
+##' @param col Color
+##' @param axes Logical. If \code{TRUE} draw axes.
+##' @param confint Logical. If \code{TRUE} draw confidence shadows.
+##' @param ... Not yet used
+##' @examples
+##' library(survival)
+##' d=sampleData(100,outcome="survival")
+##' nd=sampleData(100,outcome="survival")
+##' f1=coxph(Surv(time,event)~X1+X6+X8,data=d)
+##' f2=coxph(Surv(time,event)~X2+X5+X9,data=d)
+##' xx=Score(list(f1,f2),formula=Surv(time,event)~1,data=nd,metrics="auc")
+##' plotAUC(xx)
+#' @export
+plotAUC <- function(x,models,type="score",lwd=2,xlim,ylim,col,axes=TRUE,confint=FALSE,...){
+    times=model=AUC=lower.AUC=upper.AUC=NULL
+    pframe <- switch(type,"score"={x$AUC$score},"test"={x$AUC$test},{stop("Type has to be either 'score' for AUC or 'test' for differences in AUC.")})
+    ## plot(0,0,type="n",ylim = ylim,xlim = xlim,axes=FALSE,xlab = "Time",ylab = "AUC")
+    ## if (axes){
+    ## axis(1)
+    ## prodlim::PercentAxis(2,at=seq(ylim[1],ylim[2],(ylim[2]-ylim[1])/5))
+    ## }
+    ## pframe[,lines(times,AUC,type="l",lwd=lwd,col=col),by=model]
+    ## pframe[,dimcol:=prodlim::dimColor(col[[1]],density=55),by=model]
+    if (type=="score"){
+        if (!missing(models)) pframe <- pframe[model %in% models]
+        pframe[,col:=as.numeric(as.factor(model))]
+        pframe[,lwd:=lwd]
+        if (missing(xlim)) xlim <- pframe[,range(times)]
+        if (missing(ylim)) ylim <- c(0.5,1)
+        if (missing(col)) col <- 1:length(unique(pframe$model))
+        pp <- ggplot(data=pframe,aes(times,AUC,fill=model,colour=model))
+    }else{
+        ## test
+        pframe[,contrast:=paste(model,reference,sep=" - ")]
+        pframe[,col:=as.numeric(as.factor(contrast))]
+        pframe[,lwd:=lwd]
+        if (missing(xlim)) xlim <- pframe[,range(times)]
+        if (missing(ylim)) ylim <- c(min(pframe$lower),max(pframe$upper))
+        if (missing(col)) col <- 1:length(unique(pframe$contrast))
+        pp <- ggplot(data=pframe,aes(times,delta,fill=contrast,colour=contrast)) 
+    }
+    ## pp <- pp + theme_bw() + theme(axis.line = element_line(colour = "black"),
+    ## panel.grid.major = element_blank(),
+    ## panel.grid.minor = element_blank(),
+    ## panel.border = element_blank(),
+    ## panel.background = element_blank())
+    ## geom_segment(aes(x=xlim[1],xend=xlim[1],y=ylim[1],yend=ylim[2]))
+    ## x-axis
+    ## pp <- pp+ geom_segment(aes(x=xlim[1],xend=xlim[2],y=ylim[1],yend=ylim[1]))
+    pp <- pp+ theme_bw()
+    ## browser()
+    pp <- pp + geom_line(size=lwd) + xlim(xlim) + theme(legend.key = element_blank())
+    ## + theme_classic()
+    ## pp <- theme(axis.text.x= element_text(family, face, colour, size))
+    ## pp <- pp+theme(axis.line= element_line(colour="black", size=1,linetype="solid"))
+    ## pp <- pp+ theme(axis.line.x = element_line(color="black", size = 1),
+    ## axis.line.y = element_line(color="black", size = 1,limits=c(.6,.8)))
+    ## y-axis
+    pp <- pp + scale_y_continuous(expand=c(0,0),limits=ylim,breaks=seq(ylim[1],ylim[2],(ylim[2]-ylim[1])/4),
+                                  labels=paste(round(100*seq(ylim[1],ylim[2],(ylim[2]-ylim[1])/4),1),"%"))
+    pp
+    if (confint==TRUE){
+        if (type=="score"){
+            ## pframe[,polygon(x=c(times,rev(times)),y=c(lower.AUC,rev(upper.AUC)),col=dimcol,border=NA),by=model]
+            pp <- pp + geom_ribbon(aes(ymin=lower.AUC,ymax=upper.AUC,fill=model,linetype=NA),alpha=0.2)
+        }else{
+            pp <- pp + geom_ribbon(aes(ymin=lower,ymax=upper,fill=contrast,linetype=NA),alpha=0.2)
+        }
+    }
+    pp
+}
+
+#----------------------------------------------------------------------
+### plotAUC.R ends here
---
+++ R/plotROC.R
@@ -0,0 +1,68 @@
+### plotROC.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds
+## created: Jun 23 2016 (10:27) 
+## Version: 
+## last-updated: Jun 25 2016 (10:24) 
+##           By: Thomas Alexander Gerds
+##     Update #: 6
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+##' Plot ROC curve 
+##'
+##' @title Plot ROC curve
+#' @export
+##' @param x Object obtained with \code{Score.list}
+##' @param models Choice of models to plot
+##' @param timepoint Time point specifying the prediction horizon
+##' @param lwd line width
+##' @param legend Logical. If \code{TRUE} draw legend.
+##' @param legend.title Legend title
+##' @param ... Not yet used
+plotROC <- function(x,models,timepoint,lwd=3,legend=TRUE,legend.title,...){
+    model=FPR=TPR=times=NULL
+    pframe <- x$ROC$plotframe
+    if (!missing(models)){
+        pframe <- pframe[model%in%models]
+    }
+    setkey(pframe,model)
+    if (missing(timepoint))
+        timepoint <- max(pframe[["times"]])
+    else ## can only do one timepoint
+        timepoint <- timepoint[[1]]
+    pframe <- pframe[times==timepoint]
+    plot(0,0,type="n",ylim = 0:1,xlim = 0:1,axes=FALSE,xlab = "1-Specificity",ylab = "Sensitivity")
+    prodlim::PercentAxis(1,at=seq(0,1,.25))
+    prodlim::PercentAxis(2,at=seq(0,1,.25))
+    if (!missing(models)) pframe <- pframe[model %in% models]
+    pframe[,col:=as.numeric(as.factor(model))]
+    pframe[,lwd:=lwd]
+    pframe[,lines(c(0,FPR,1),c(0,TPR,1),type="l",lwd=lwd,col=col),by=model]
+    if (legend!=FALSE){
+        if (!is.character(legend)){
+            if (!missing(models))
+                auc <- x$AUC$score[(times==timepoint) & (model%in%models)]
+            else
+                auc <- x$AUC$score[(times==timepoint)]
+            setkey(auc,model)
+            ## legend <- paste0(auc$model,": ",Publish::formatCI(100*auc$AUC,100*auc$lower.AUC,100*auc$upper.AUC,showX=TRUE,digits=1))
+            legend <- paste0(auc$model,": ",sprintf(fmt="%s [%s;%s]",
+                                                    round(100*auc$AUC,digits=1),
+                                                    round(100*auc$lower.AUC,digits=1),
+                                                    round(100*auc$upper.AUC,digits=1)))
+        }
+        if (missing(legend.title)) legend.title <- "AUC"
+        pframe[,legend(x="bottomright",legend=legend,lwd=lwd[[1]],title=legend.title,col=unique(col))]
+    }
+    abline(a=0,b=1,col="gray77",lwd=3)
+    invisible(x)
+}
+
+#----------------------------------------------------------------------
+### plotROC.R ends here
