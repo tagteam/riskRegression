e79dbeaad1b2ef6d4dd3e52fb5c203881d1c9326
Thomas Alexander Gerds <tag@biostat.ku.dk>
2017-02-11 10:04:08 +0100

    new print function

---
+++ R/print.predictCSC.R
@@ -0,0 +1,25 @@
+### print.predictCSC.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds
+## created: Feb 11 2017 (10:01) 
+## Version: 
+## last-updated: Feb 11 2017 (10:03) 
+##           By: Thomas Alexander Gerds
+##     Update #: 2
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+#' @method print predictCSC
+#' @export
+print.predictCSC <- function(x,...){
+    
+}
+
+
+#----------------------------------------------------------------------
+### print.predictCSC.R ends here
