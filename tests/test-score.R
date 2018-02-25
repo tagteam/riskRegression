faa3032047e30575c47397c26d4537b1083c9af0
Thomas Alexander Gerds <tag@biostat.ku.dk>
2018-02-25 08:59:40 +0100

    changed one more instance ipa to IPA

--- tests/test-score.R
+++ tests/test-score.R
@@ -3,9 +3,9 @@
 ## author: Thomas Alexander Gerds
 ## created: Jan  4 2016 (14:30) 
 ## Version: 
-## last-updated: Feb 23 2018 (17:17) 
+## last-updated: Feb 25 2018 (08:59) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 88
+##     Update #: 89
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -30,7 +30,7 @@ test_that("R squared/IPA", {
     f1 <- glm(Y~X1+X5+X8,data=d, family="binomial")
     f2 <- glm(Y~X2+X6+X9+X10,data=d, family="binomial")
     r1 <- rsquared(f1,newdata=d)
-    r2 <- ipa(f2,newdata=d)
+    r2 <- IPA(f2,newdata=d)
     full <- Score(list(f1=f1,f2=f2),formula=Y~1,data=d,conf.int=TRUE,summary=c("RR"),plots="ROC")
     expect_equal(r1$IPA.gain[1],full$Brier$score[model=="f1",IPA])
     expect_equal(r2$IPA[2],full$Brier$score[model=="f2",IPA])
