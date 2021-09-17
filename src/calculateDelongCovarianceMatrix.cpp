a8d1de20e5d9bee8c3f6c78b175b3e5d6495aa7e
eestet75 <jo-se-oh@hotmail.com>
2021-09-17 14:20:08 +0200

    Initial commit of calculateDelongCovarianceFast, not fully tested

--- src/calculateDelongCovarianceMatrix.cpp
+++ src/calculateDelongCovarianceMatrix.cpp
@@ -3,10 +3,106 @@
 using namespace Rcpp;
 using namespace arma;
 
-// Calculates the asymptotic covariance matrix for the delongtest function.
-// First function is useful in the case where dolist has length 0 and we don't want the entire covariance matrix
+// C++ functions for calculating the asymptotic covariance matrix for the delongtest function.
+
+// calculates midrank
+vec calculateMidrank(vec z){
+  int m = z.size();
+  vec wtemp = sort(z);
+  uvec index = sort_index(z);
+  vec w(z.size()+1);
+  for (int i = 0; i <m;i++ ){
+    w[i]=wtemp[i];
+  }
+  w[m]=wtemp[m-1]+1;
+  vec t(m,fill::zeros);
+  int i = 0;
+  int a, b, j;
+  while (i < m){
+    a = i;
+    j = a;
+      while (w[j]==w[a]){
+        j+=1;
+      }
+      b = j - 1;
+    for (int k = a; k <= b; k++){
+      t[k] = (double)(a+b)/2;
+    }
+    i = b+1;
+  }
+  int k;
+  vec tk(m,fill::zeros);
+  for (i = 0; i < m; i++){
+    k = index[i];
+    tk[k] = t[i];
+  }
+  return tk;
+}
+
+// Fast implementation of the calculation of the covariance matrix.
+// Number of rows is the number of observations, for X and Y respectively
+// Number of columns is the number of experiments
+// note that many of the copying methods may be slow and will need to be optimized
+// we note here that pointers and references should be used in order
+// to make efficient use of the memory
 // [[Rcpp::export]]
-NumericVector delongtestHelper(int nauc, int nCases, int nControls, NumericMatrix tmn, NumericMatrix tmp){
+NumericMatrix calculateDelongCovarianceFast(NumericMatrix Xs, NumericMatrix Ys){
+  int m = Xs.nrow();
+  int n = Ys.nrow();
+
+  assert(Xs.ncol()==Ys.ncol());
+  int k = Xs.ncol();
+  mat V10(k,m);
+  mat V01(k,n);
+  vec theta(k,fill::zeros);
+  for (int r = 0; r < k; r++){
+    //theta[r] = 0.0;
+    // Make them into armadillo vectors; might be an inefficient and superfluous operation
+    // strangely enough we cannot write Xr = as<vec>(Xs(_,r))
+    NumericVector Xx = Xs(_,r);
+    NumericVector Yy = Ys(_,r);
+    vec Xr = as<vec>(Xx);
+    vec Yr = as<vec>(Yy);
+    // concatenate
+    vec Zr = join_cols(Xr,Yr);
+    // calculate midranks
+    vec TZr = calculateMidrank(Zr);
+    vec TXr = calculateMidrank(Xr);
+    vec TYr = calculateMidrank(Yr);
+    for (int i = 0; i < m; i++){
+      // FIgure out how Xr is ordered compared to Zr ???
+      //?1=TZr[i];
+      //
+      V10(r,i)=(TZr[i]-TXr[i])/((double) n);
+      theta[r]+=TZr[i];
+    }
+    theta[r]=theta[r]/(double (m*n))-(double) (m+1)/(2*n);
+    for (int j = 0; j < n; j++){
+      // FIgure out how Yr is ordered compared to Zr ???
+      //?2=TZr[m+j];
+      V01(r,j)=1.0-(TZr[j]-TYr[j])/((double) m);
+    }
+  }
+  mat S(k,k);
+  for (int r = 0; r < k; r++){
+    for (int s = 0; s < k; s++){
+      double s10, s01 = 0.0;
+      for (int i = 0; i < m; i++){
+        s10+=(V10(r,i)-theta[r])*(V10(s,i)-theta[s]);
+      }
+      for (int j = 0; j < n; j++){
+        s01 += (V01(r,j)-theta[r])*(V01(s,j)-theta[s]);
+      }
+      S(r,s)=s10/((double) (m-1))+ s01/(double(n-1));
+    }
+  }
+  return wrap(S);
+}
+
+
+// Function is useful in the case where dolist has length 0 and we don't want the entire covariance matrix
+// [[Rcpp::export]]
+NumericVector calculateDelongDiagonal(int nauc, int nCases, int nControls, NumericMatrix tmn, NumericMatrix tmp){
   //std::cout << nauc << "\n\n";;
   NumericVector SEOfS(nauc);
   for (int j = 0; j < nauc; j++){
@@ -50,7 +146,10 @@ NumericVector delongtestHelper(int nauc, int nCases, int nControls, NumericMatri
   return wrap(SEOfS);
 }
 
-
+/*Pure rewrite of
+for (i in 1:nCases) {
+    V10[i, ] <- rowSums(tmn < tmp[, i]) + 0.5 * rowSums(tmn == tmp[, i])
+}*/
 // [[Rcpp::export]]
 NumericMatrix rowSumsAlt1(NumericMatrix V, NumericMatrix tmn, NumericMatrix tmp) {
   for (int r = 0; r < V.nrow(); r++) {
@@ -72,6 +171,10 @@ NumericMatrix rowSumsAlt1(NumericMatrix V, NumericMatrix tmn, NumericMatrix tmp)
   return wrap(V);
 }
 
+/*Pure rewrite of
+ for (i in 1:nControls) {
+    V01[i, ] <- rowSums(tmp > tmn[, i]) + 0.5 * rowSums(tmp == tmn[, i])
+ }*/
 // [[Rcpp::export]]
 NumericMatrix rowSumsAlt2(NumericMatrix V, NumericMatrix tmn, NumericMatrix tmp) {
   for (int r = 0; r < V.nrow(); r++) {
