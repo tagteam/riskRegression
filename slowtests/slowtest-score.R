a82ef02804397ec93c68474be53cf7c986a3aa9b
Thomas Alexander Gerds <tag@biostat.ku.dk>
2021-10-15 10:56:41 +0200

    nothing

--- slowtests/slowtest-score.R
+++ slowtests/slowtest-score.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Dec  6 2020 (09:25) 
 ## Version: 
-## Last-Updated: Dec  6 2020 (09:31) 
+## Last-Updated: Oct 11 2021 (11:47) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 2
+##     Update #: 3
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -93,6 +93,7 @@ test_that("survival outcome: Brier Score pec vs Score",{
     d <- sampleData(43,outcome="survival")
     f1 <- coxph(Surv(time,event)~X1+X5+X8,data=d, x = TRUE, y = TRUE)
     f2 <- coxph(Surv(time,event)~X2+X6+X9+X10,data=d, x = TRUE, y = TRUE)
+    library(pec)
     p1 <- pec(list(f1,f2),formula=Surv(time,event)~1,data=d,times=c(3,5,10),exact=FALSE,start=NULL)
     s1 <- Score(list(f1,f2),formula=Surv(time,event)~1,data=d,times=c(3,5,10),conf.int=FALSE,metrics="brier")
     expect_equal(p1$AppErr$coxph,s1$Brier$score[model=="coxph",Brier])
@@ -244,57 +245,57 @@ test_that("vcov AUC",{
 # }}}
 
 
-    test_that("loob binary",{
-        learndat=sampleData(200,outcome="binary")
-        lr1a = glm(Y~X6,data=learndat,family=binomial)
-        lr2a = glm(Y~X7+X8+X9,data=learndat,family=binomial)
-        ## leave-one-out bootstrap
-        ## in series
-        set.seed(5)
-        system.time(loob.se0 <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,se.fit=TRUE))
-        ## in multicore 3 cpu's 
-        set.seed(5)
-        system.time(loob.se0a <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,parallel="multicore",ncpus=3,se.fit=TRUE))
-        ## snow 3 cpu's 
-        library(parallel)
-        mycl <- parallel::makePSOCKcluster(rep("localhost", 3))
-        set.seed(5)
-        system.time(loob.se0b <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,parallel="snow",cl=mycl,se.fit=TRUE))
-        try(stopCluster(mycl),silent=TRUE)
-        set.seed(5)
-        loob.se1 <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,se.fit=FALSE)
-        expect_equal(loob.se0$AUC$contrasts$delta,loob.se0b$AUC$contrasts$delta)
-        expect_equal(loob.se0$AUC$contrasts$delta,loob.se0a$AUC$contrasts$delta)
-        expect_equal(loob.se0$AUC$contrasts$delta,loob.se1$AUC$contrasts$delta)
-        expect_equal(loob.se0$Brier$contrasts$delta,loob.se1$Brier$contrasts$delta)
-    })
+test_that("loob binary",{
+    learndat=sampleData(200,outcome="binary")
+    lr1a = glm(Y~X6,data=learndat,family=binomial)
+    lr2a = glm(Y~X7+X8+X9,data=learndat,family=binomial)
+    ## leave-one-out bootstrap
+    ## in series
+    set.seed(5)
+    system.time(loob.se0 <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,se.fit=TRUE))
+    ## in multicore 3 cpu's 
+    set.seed(5)
+    system.time(loob.se0a <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,parallel="multicore",ncpus=3,se.fit=TRUE))
+    ## snow 3 cpu's 
+    library(parallel)
+    mycl <- parallel::makePSOCKcluster(rep("localhost", 3))
+    set.seed(5)
+    system.time(loob.se0b <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,parallel="snow",cl=mycl,se.fit=TRUE))
+    try(stopCluster(mycl),silent=TRUE)
+    set.seed(5)
+    loob.se1 <- Score(list("LR1"=lr1a,"LR2"=lr2a),formula=Y~1,data=learndat,split.method="loob",B=100,se.fit=FALSE)
+    expect_equal(loob.se0$AUC$contrasts$delta,loob.se0b$AUC$contrasts$delta)
+    expect_equal(loob.se0$AUC$contrasts$delta,loob.se0a$AUC$contrasts$delta)
+    expect_equal(loob.se0$AUC$contrasts$delta,loob.se1$AUC$contrasts$delta)
+    expect_equal(loob.se0$Brier$contrasts$delta,loob.se1$Brier$contrasts$delta)
+})
 
-    test_that("bootcv binary (multi.state.test)",{
-        learndat=sampleData(200,outcome="binary")
-        lr1a = glm(Y~X6,data=learndat,family=binomial)
-        lr2a = glm(Y~X7+X8+X9,data=learndat,family=binomial)
-        ## leave-one-out bootstrap
-        set.seed(5)
-        bootcv.se0 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=FALSE,multi.split.test=FALSE,metric="brier")
-        set.seed(5)
-        bootcv.se1 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=TRUE,multi.split.test=FALSE,metric="brier")
-        set.seed(5)
-        bootcv.se2 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=FALSE,multi.split.test=TRUE,metric="brier")
-        set.seed(5)
-        bootcv.se3 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=TRUE,multi.split.test=TRUE,metric="brier")
-        bootcv <- list(bootcv.se0,bootcv.se1,bootcv.se2,bootcv.se3)
-        ## delta
-        for (i in 1:4)
-            for (j in 2:4) 
-                for (m in c("Brier"))
-                    expect_equal(bootcv[[i]][[m]]$contrasts$delta,bootcv[[j]][[m]]$contrasts$delta)
-        ## lower, upper
-        expect_equal(bootcv[[2]][["Brier"]]$contrasts[,.(lower,upper)],bootcv[[4]][["Brier"]]$contrasts[,.(lower,upper)])
-        ## p-value (does not work yet)
-        ## for (m in c("AUC","Brier")){
-        ## expect_equal(bootcv[[3]][[m]]$contrasts[,.(p)],bootcv[[4]][[m]]$contrasts[,.(p)])
-        ## }
-    })
+test_that("bootcv binary (multi.state.test)",{
+    learndat=sampleData(200,outcome="binary")
+    lr1a = glm(Y~X6,data=learndat,family=binomial)
+    lr2a = glm(Y~X7+X8+X9,data=learndat,family=binomial)
+    ## leave-one-out bootstrap
+    set.seed(5)
+    bootcv.se0 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=FALSE,multi.split.test=FALSE,metric="brier")
+    set.seed(5)
+    bootcv.se1 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=TRUE,multi.split.test=FALSE,metric="brier")
+    set.seed(5)
+    bootcv.se2 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=FALSE,multi.split.test=TRUE,metric="brier")
+    set.seed(5)
+    bootcv.se3 <- Score(list("LR1"=lr1a,"LR2"=lr2a),conservative=TRUE,formula=Y~1,data=learndat,split.method="bootcv",B=10,se.fit=TRUE,multi.split.test=TRUE,metric="brier")
+    bootcv <- list(bootcv.se0,bootcv.se1,bootcv.se2,bootcv.se3)
+    ## delta
+    for (i in 1:4)
+        for (j in 2:4) 
+            for (m in c("Brier"))
+                expect_equal(bootcv[[i]][[m]]$contrasts$delta,bootcv[[j]][[m]]$contrasts$delta)
+    ## lower, upper
+    expect_equal(bootcv[[2]][["Brier"]]$contrasts[,.(lower,upper)],bootcv[[4]][["Brier"]]$contrasts[,.(lower,upper)])
+    ## p-value (does not work yet)
+    ## for (m in c("AUC","Brier")){
+    ## expect_equal(bootcv[[3]][[m]]$contrasts[,.(p)],bootcv[[4]][[m]]$contrasts[,.(p)])
+    ## }
+})
 
 # {{{ "Brier score"
 test_that("Brier score",{
