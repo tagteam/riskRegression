d0cf63218060fb038cf121c2af4d08f4f13c4f50
eestet75 <jo-se-oh@hotmail.com>
2022-05-17 14:30:36 +0200

    added generic getCensoringWeights function so that it works for more general censoring models

--- slowtests/test-score-se.R
+++ slowtests/test-score-se.R
@@ -1,5 +1,5 @@
 #AUC with censoring covariates example
-## devtools::load_all()
+devtools::load_all()
 library(riskRegression)
 library(testthat)
 library(cmprsk)
@@ -12,7 +12,7 @@ test_that("AUC, covariates in censoring, competing risk, also an example of hal9
   x<-Score(list("CSC(X1+X2+X7+X9)"=csc1,"CSC(X1+X2)"=csc2),
            formula=Hist(time,event)~X2+X7,data=testCR.comprisk,se.fit=1L,times=c(4),metrics="AUC",conservative = TRUE)
   y<-Score(list("CSC(X1+X2+X7+X9)"=csc1,"CSC(X1+X2)"=csc2),
-           formula=Hist(time,event)~X2+X7,data=testCR.comprisk,se.fit=1L,times=c(4),metrics="AUC",cens.model="hal9001",conservative = TRUE)
+           formula=Hist(time,event)~X2+X7,data=testCR.comprisk,se.fit=1L,times=c(4),metrics="AUC",cens.model="Hal9001",conservative = TRUE)
   expect_output(print(x))
 })
 
