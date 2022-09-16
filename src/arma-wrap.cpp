d5db28cb1ba416346d7ac45fd7ba2947c1477697
eestet75 <jo-se-oh@hotmail.com>
2022-09-16 12:06:47 +0200

    Added code to check vectors have the same lengths and that they do not include NAs in the cpp files.

---
+++ src/arma-wrap.cpp
@@ -0,0 +1,23 @@
+#include "arma-wrap.h"
+
+using namespace arma;
+
+void checkNAs(Rcpp::NumericVector& vec, std::string var_name){
+  for (int i=0; i< vec.size(); i++) {
+    if (R_IsNA(vec[i])) {
+      Rcpp::stop("NA(s) in %i. ", var_name); // Calls R function for stopping
+    }
+  }
+}
+
+void checkNAs(double val, std::string var_name){
+  if (isnan(val)){
+    Rcpp::stop("Variable %i is NA. ", var_name);
+  }
+}
+
+void compareLengths(Rcpp::NumericVector& vec1, Rcpp::NumericVector& vec2){
+  if (vec1.size() != vec2.size()){
+    Rcpp::stop("Some vectors have unequal lengths. ");
+  }
+}
