0f76e0bf4d0765656fdc7b1c79e3f70305ba67f2
eestet75 <jo-se-oh@hotmail.com>
2023-06-29 17:04:01 +0200

    Fix binary case for SeSpPPVNPV. Add support for  survival case.

--- R/AUC.survival.R
+++ R/AUC.survival.R
@@ -15,7 +15,7 @@
 ##
 ### Code:
 
-AUC.survival <- function(DT,breaks=NULL,MC,se.fit,conservative,cens.model,keep.vcov=FALSE,multi.split.test,alpha,N,NT,NF,dolist,ROC,IC.data,...){
+AUC.survival <- function(DT,breaks=NULL,MC,se.fit,conservative,cens.model,keep.vcov=FALSE,multi.split.test,alpha,N,NT,NF,dolist,ROC,IC.data,cutpoints,...){
     ID=model=times=risk=Cases=time=status=Controls=TPR=FPR=WTi=Wt=ipcwControls=ipcwCases=IF.AUC=lower=se=upper=AUC=nth.times=NULL
     cause <- 1
     aucDT <- DT[model>0]
@@ -41,7 +41,64 @@ AUC.survival <- function(DT,breaks=NULL,MC,se.fit,conservative,cens.model,keep.v
       sum((FP-c(0,FP[-N]))*((c(0,TP[-N])+TP)/2))
     }
     score <- aucDT[nodups,list(AUC=AireTrap(FPR,TPR)),by=list(model,times)]
-    if (ROC==TRUE) {
+    if (!is.null(cutpoints)){
+      breaks <- sort(cutpoints,decreasing = TRUE)
+      aucDT[,nth.times:=as.numeric(factor(times))]
+      helper.fun <- function(FPR,TPR,risk,ipcwCases,ipcwControls, N, time,times,event,cens.model,nth.times,conservative, IC.G, cutpoints){
+        den_TPR<-sum(ipcwCases) ## estimate the cumulative incidence via IPCW
+        den_FPR<-sum(ipcwControls)
+        indeces <- sindex(risk,cutpoints,comp = "greater",TRUE)
+        res <- list()
+        ordered <- order(time) ## can probably move this outside to improve computation time, for now keep it
+        for (i in 1:length(cutpoints)){
+          den_PPV <- sum(ipcwCases[risk > cutpoints[i]]+ipcwControls[risk > cutpoints[i]])
+          den_NPV <- 1-den_PPV
+          if (indeces[i] != 0){
+            TPRi <- TPR[indeces[i]]
+            FPRi <- FPR[indeces[i]]
+            IC0.TPR <- ipcwCases*N*((risk > cutpoints[i])-TPRi)/den_TPR
+            SE.TPR <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.TPR[ordered],IC0.TPR[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+            IC0.FPR <- (ipcwControls)*N*((risk > cutpoints[i])-FPRi)/(1-den_TPR)
+            SE.FPR <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.FPR[ordered],IC0.FPR[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+          }
+          else {
+            TPRi <- FPRi <- 0
+            SE.TPR <- SE.FPR <- NA
+          }
+          if (den_PPV > 1e-10){
+            PPV <- (TPRi*den_TPR)/den_PPV
+            IC0.PPV <- (risk > cutpoints[i])/den_PPV*(((ipcwCases)*N)*(1*(event==1)-1*(event!=0)*PPV)-ipcwControls*N*PPV) #OBS, check other causes, paul's implementation
+            SE.PPV <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.PPV[ordered],IC0.PPV[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+            
+            ## Alternative implementation
+            # IC0.PPV <- (risk > cutpoints[i])/den_PPV*(ipcwCases*N - PPV)
+            # weights.PPV <- ((risk > cutpoints[i])/(den_PPV))*((1-PPV)*ipcwCases*N - PPV*(N*ipcwControls1+N*ipcwControls2)) #include censoring from denominator
+            # weights.PPV <- ((risk > cutpoints[i])/(den_PPV))*(ipcwCases*N) #exclude censoring from denominator
+            # SE.PPV <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.PPV[ordered],weights.PPV[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+          }
+          else {
+            PPV <- NA
+          }
+          if (den_NPV > 1e-10){
+            NPV <- ((1-FPRi)*den_FPR)/den_NPV
+            IC0.NPV <- (risk <= cutpoints[i])/den_NPV*(((ipcwCases)*N)*(1*(event!=1 & event!=0)-1*(event!=0)*NPV)+ipcwControls*N*(1-NPV)) #OBS, check other causes, paul's implementation
+            SE.NPV <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.NPV[ordered],IC0.NPV[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+            
+            ## Alternative implementation
+            # IC0.NPV <- (risk <= cutpoints[i])/den_NPV*((ipcwControls1+ipcwControls2)*N - NPV)
+            # weights.NPV <- ((risk <= cutpoints[i])/(den_NPV))*((ipcwControls1+ipcwControls2)*N)
+            # SE.NPV <- sd(getInfluenceCurve.Brier(times,time[ordered],IC0.NPV[ordered],weights.NPV[ordered],IC.G,cens.model,nth.times,conservative,event[ordered]))/sqrt(N)
+          }
+          else {
+            NPV <- NA
+          }
+          res[[i]] <- data.table(risk = cutpoints[i], TPR=TPRi, SE.TPR=SE.TPR,FPR=FPRi, SE.FPR=SE.FPR,PPV=PPV,SE.PPV=SE.PPV,NPV=NPV,SE.NPV=SE.NPV)
+        }
+        do.call("rbind",res)
+      }
+      output <- list(res.cut=aucDT[, helper.fun(FPR,TPR,risk,ipcwCases,ipcwControls, N, time,times[1],status,cens.model,nth.times[1],conservative, MC, cutpoints),by=list(model,times)])
+    }
+    else if (ROC==TRUE) {
       if (is.null(breaks)){
         output <- list(ROC=aucDT[nodups,c("model","times","risk","TPR","FPR"),with=FALSE])
       }
