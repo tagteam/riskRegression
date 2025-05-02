205b80c4d660854f8107afbb60724d6f2ad953ae
Thomas Alexander Gerds <tag@biostat.ku.dk>
2025-05-02 13:27:17 +0200

    merging coxnet with GLMnet

--- R/coxnet.R
+++ R/coxnet.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Apr 27 2025 (08:29) 
 ## Version: 
-## Last-Updated: Apr 29 2025 (14:32) 
+## Last-Updated: May  2 2025 (12:32) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 7
+##     Update #: 25
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -20,42 +20,67 @@
 ##' and enhances the resulting object 
 ##' @title
 ##' @param formula Formula where the left hand side specifies the event
-#' history and the right hand side the linear predictor.  
+##' history and the right hand side the linear predictor.  
 ##' @param data The data on which to fit the model. 
-##' @param lambda The tuning parameters for GLMnet. If set to NULL, then it the parameters are chosen for you.
-##' @param alpha
-##' @param cv
 ##' @param strata
-##' @param ... 
-##' @return 
-##' @seealso 
-##' @examples 
+##' @param lambda The tuning parameters for GLMnet. If set to NULL, then it the parameters are chosen for you.
+#' @param alpha The elasticnet mixing parameter. 
+##' @param nfolds Number of folds for cross-validation. Default is 10.
+##' @param ...  Additional arguments that are passed on to glmnet.
+##' @return cox net object
+##' @seealso \link[glmnet]{glmnet}
+##' @examples
+##' d <- sampleData(73,outcome="survival")
+##' f <- coxnet(Surv(time,event)~X1+X2+X3+X4+X5+X6+X7+X8+X9+X10,data=d)
+##' f1 <- coxnet(Surv(time,event)~strata(X1)+X2+X3+X4+X5+X6+X7+X8+X9+X10,data=d)
 ##' @export 
 ##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
 coxnet <- function(formula,
                    data,
-                   lambda=0.1,
+                   lambda=NULL,
                    alpha=1,
                    cv=FALSE,
-                   strata=NULL,
+                   nfolds = 10,
                    ...){
+    requireNamespace("glmnet")
+    unpenalized <- function(x)x
     # alpha = 1 equals lasso, alpha = 0 equals ridge, and alpha in (0,1) equals elastic net
-    ehf <- prodlim::EventHistory.frame(formula, data, specials = NULL)
-    if(length(strata)>0){
-        ehf$event.history <- glmnet::stratifySurv(ehf$event.history, strata = data[[strata]])
+    EHF <- prodlim::EventHistory.frame(formula, data, specials = c("strata","unpenalized"))
+    if(length(EHF$strata)>0){
+        attr(EHF$event.history,"strata") <- EHF$strata
+        class(EHF$event.history) <- c("stratifySurv",class(EHF$event.history))
     }
-    if(cv){
-        gfit <- glmnet::cv.glmnet(y = ehf$event.history, x = ehf$design, alpha = alpha, family = "cox",...)
-    } else{
-        gfit <- glmnet::glmnet(y = ehf$event.history, x = ehf$design, alpha = alpha, lambda = lambda, family = "cox",...)
+    if (length(EHF$unpenalized)>0){
+        penalty.factor <- c(rep(1,NCOL(EHF$design)),rep(0,NCOL(EHF$unpenalized)))
+        X <- cbind(EHF$design,EHF$unpenalized)
+    }else{
+        X <- EHF$design
+        penalty.factor <- rep(1,NCOL(EHF$design))
     }
-    gfit$design <- ehf$design
-    gfit$y <- ehf$event.history
-    gfit$formula <- formula
-    gfit$strata <- strata
-    gfit$cv <- cv
-    class(gfit) <- c(class(gfit), "coxnet")
-    gfit
+    args <- list(y = EHF$event.history,
+                 x = X,
+                 alpha = alpha,
+                 family = "cox",
+                 nfolds=nfolds,
+                 penalty.factor = penalty.factor,
+                 ...)
+    if (cv){
+        cv.glmnet(y = EHF$event.history,
+                  x = X,
+                  alpha = alpha,
+                  family = "cox",
+                  nfolds=nfolds,
+                  penalty.factor = penalty.factor,
+                  ...)
+    }else{
+        args$lambda <- lambda
+        fun <- "glmnet"
+    }
+    fit <- do.call(what = fun ,args = args)
+    fit$design <- EHF$design
+    fit$strata <- strata
+    fit$terms = terms(formula)
+    fit
 }
 
 ######################################################################
