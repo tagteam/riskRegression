98d1e2456fa5416fb5dcc87ea8de6dee329f82a8
Thomas Alexander Gerds <tag@biostat.ku.dk>
2026-02-26 14:34:52 +0100

    fixed testfiles

--- slowtests/test-CSC-GLMnet.R
+++ slowtests/test-CSC-GLMnet.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Apr 28 2025 (09:31) 
 ## Version: 
-## Last-Updated: feb 14 2026 (08:20) 
+## Last-Updated: feb 26 2026 (13:42) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 13
+##     Update #: 16
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -16,6 +16,7 @@
 ### Code:
 library(riskRegression)
 library(testthat)
+library(survival)
 library(glmnet)
 test_that("unpenalized Cox models",{
     set.seed(17)
@@ -52,7 +53,33 @@ test_that("penalized Cox models",{
     riskRegression::predictRisk(l,newdata = test,times = 3)
     ## plotRisk(x,times = 3)
     ## plotRisk(x,times = 3,models = c("lasso","elnet"))
-    x
+    suppressMessages(expect_output(print(x)))
+})
+
+test_that("Cox models with block penaly",{
+    set.seed(17)
+    d <- sampleData(100)
+    test <- sampleData(1000)
+    # unpenalized
+    a <- CSC(list(Hist(time,event)~unpenalized(X1)+X6+pen(X7,3)+X8,Hist(time,event)~X1+X6+X9),data=d,fitter = c("glmnet","coxph"))
+    # ridge
+    b <- CSC(list(Hist(time,event)~X1+X6+X7+X8,Hist(time,event)~X1+X6+X9),data=d,fitter = "glmnet",
+             alpha = 0)
+    # elnet
+    e <- CSC(list(Hist(time,event)~X1+X6+X7+X8,Hist(time,event)~X1+X6+X9),data=d,fitter = "glmnet",
+             alpha = 0.5)
+    # lasso
+    l <- CSC(list(Hist(time,event)~X1+X6+X7+X8,Hist(time,event)~X1+X6+X9),data=d,fitter = "glmnet",
+             alpha = 1)
+    x = Score(list(unpenalized = a,rigde = b, elnet = e,lasso = l),
+              formula = Hist(time,event)~1,
+              data = test,
+              summary = "risk",
+              times = 3)
+    riskRegression::predictRisk(l,newdata = test,times = 3)
+    ## plotRisk(x,times = 3)
+    ## plotRisk(x,times = 3,models = c("lasso","elnet"))
+    suppressMessages(expect_output(print(x)))
 })
 
 
